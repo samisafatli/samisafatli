137210af5c0594faa428b5fa1000f6ee65181261
Sami Safatli <sami.safatli@yduqs.com.br>
2023-11-08 16:23:40 -0300

    Add timeline data

---
+++ src/data/timeline.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        year: 'June 2023 - Present',
+        title: 'YDUQS - Sr Software Engineer',
+        duration: '6 months',
+        details: `I'm currently working in the biggest university app in Brazil. I'm responsible for the whole student path in the application, since the onboarding, having classes, sharing documents and finances. Using Agile methodologies, code review and other good engineering practices. The app is built with ReactNative.`
+    },
+    {
+        year: 'September 2022 - March 2023',
+        title: 'Paypal - Sr Software Engineer',
+        duration: '1 year and 6 months',
+        details: `As a consultant I worked with React, Typescript and Cypress in two different teams, Crowdfunding and PayPal.Me. Those two features are being used worldwide, I participated in the migration of the application.`
+    },
+
+    {
+        year: 'March 2021 - August 2021',
+        title: 'Globo - Software Engineer',
+        duration: '5 months',
+        details: `I worked in the Content team, responsible for creating the CMS, which is an environment for editors to create different types of content. In the team, I work mainly with React development.`
+    },
+
+    {
+        year: 'August 2019 - March 2021',
+        title: 'Globo - Senior Software Engineer',
+        duration: '1 year and 7 months',
+        details: `Using NodeJS, Golang and Python for the Backend and ReactJS for the Frontend, I worked in several projects. Initially in the Interactivity team, taking care of Votes and Comments for the entire Globo platform. Soon after, I worked in the Distribution team that delivers content to all Homes.`
+    },
+
+    {
+        year: 'December 2018 - August 2019',
+        title: 'LogPlanning - Developer Intern',
+        duration: '8 months',
+        details: `I was responsible for understanding the needs of the Client (Ipiranga), maintaining and correcting routines and scripts, in addition to creating services, using HTML, CSS, JS and Caché.`
+    },
+]
