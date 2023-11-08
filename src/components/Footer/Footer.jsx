09ff330c4f20eff41c83293259a1c67e82d87a52
Sami Safatli <sami.safatli@yduqs.com.br>
2023-11-08 16:24:35 -0300

    Add Contact and Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+	return (
+		<div className="py-5 text-center">
+			<p className="text-sm mt-2  opacity-50">
+				&copy; {new Date().getFullYear()} Sami Safatli. All rights reserved.
+			</p>
+		</div>
+	);
+}
+
+export default Footer;
