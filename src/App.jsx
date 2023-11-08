0ed78b5c12d489cacbdbe7375fba6bf5f2f10ee9
Sami Safatli <sami.safatli@yduqs.com.br>
2023-11-08 16:22:11 -0300

    Add components structure inside App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import Intro from './components/Intro/Intro'
+import Portfolio from './components/Portfolio/Portfolio'
+import Timeline from './components/Timeline/Timeline'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
+        <div className="max-w-5xl w-11/12 mx-auto">
+          <Intro />
+          <Portfolio />
+          <Timeline />
+          <Contact />
+          <Footer />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
