6f45106a03f0e23e7260289964ce9b9fda2cfda1
Sami Safatli <sami.safatli@yduqs.com.br>
2023-11-08 16:23:51 -0300

    Add portfolio data

---
+++ src/data/portfolio.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: 'Github',
+        imgUrl: '/assets/images/image_1.png',
+        stack: ['Personal Github'],
+        link: 'https://github.com/samisafatli',
+    },
+
+    {
+        title: 'Linkedin',
+        imgUrl: '/assets/images/image_2.png',
+        stack: ['Linkedin Profile'],
+        link: 'https://www.linkedin.com/in/safatlisami/',
+    },
+
+    {
+        title: 'AL AI',
+        imgUrl: '/assets/images/image_3.png',
+        stack: ['React, Javascript, CSS'],
+        link: 'https://google.com.br',
+    }
+]
